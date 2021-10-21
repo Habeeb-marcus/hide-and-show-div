125781168f5f536a8b6c34c5aae6b503b3b74f44
mac <h.makusota@outlook.com>
2021-10-21 17:11:48 +0100

    onload event added

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
+const setUpEvents = () => {
 const main = document.getElementById('main');
 const btn = document.getElementById('btn');
-
-
-
-// console.log(btn);
-
-
 const toggleBox = () => {
         // console.log('show and hide me');
     if(main.classList.contains('open')){
         main.classList.remove('open')
-        btn.innerHTML = "show more"
-
-        
+        btn.innerHTML = "show more"       
     } else {
         main.classList.add('open')
         btn.innerHTML = "show less"
-
     }
+}
 
+btn.addEventListener('click', toggleBox)
 }
 
 
 
+ window.onload = function () {
 
+        setUpEvents()
+    }
 
-btn.addEventListener('click', toggleBox)
