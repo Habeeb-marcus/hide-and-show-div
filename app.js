ba0fcefbc43fde85b1e5f19a0619100758cc0b10
mac <h.makusota@outlook.com>
2021-10-26 19:42:33 +0100

    setinterval and clearainterval

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const setUpEvents = () => {
+// const setUpEvents = () => {
 const main = document.getElementById('main');
 const btn = document.getElementById('btn');
+var color = ["green", "red", "blue", "tomato", "orange", "brown", "gray"];
+let counter = 0;
+
 const toggleBox = () => {
         // console.log('show and hide me');
     if(main.classList.contains('open')){
@@ -12,13 +15,36 @@ const toggleBox = () => {
     }
 }
 
-btn.addEventListener('click', toggleBox)
+const changeColor = () => {
+
+    if(counter >= color.length) {
+        counter = 0
+    }
+    main.style.backgroundColor = color[counter];
+    counter++;
+}
+
+var timer = setInterval(changeColor, 3000);
+
+// console.log(counter);
+
+const stopColor = () => {
+    if(main.classList.contains('open')){
+    clearInterval(timer)
+    } else {
+       changeColor()
+       setInterval(changeColor, 3000)
+    }
 }
 
 
+btn.addEventListener('click', toggleBox)
+btn.addEventListener('click', stopColor)
+// }
 
- window.onload = function () {
 
-        setUpEvents()
-    }
+
+//  window.onload = function () {
+//         setUpEvents()
+//     }
 
